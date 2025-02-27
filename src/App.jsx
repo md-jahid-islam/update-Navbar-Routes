3cc0c159e3eaf7d6c317b6ff74e8dff56e169b2b
Jahidul Islam <jahidulislamwebbd@gmail.com>
2025-02-28 01:44:33 +0600

    colors change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-
 import './App.css'
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from './Pages/Home';
 import Aboute from './Pages/Aboute';
@@ -10,10 +8,10 @@ import Contact from './Pages/Contact';
 import Layout from './Components/layoouts/Layout';
 
 function App() {
-  const root = document.getElementById("root");
 
-  ReactDOM.createRoot(root).render(
-    <BrowserRouter>
+  return (
+    <>
+ <BrowserRouter>
       <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
@@ -23,11 +21,6 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-  );
-
-  return (
-    <>
-
     </>
   )
 }
